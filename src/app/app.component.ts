0182e4420566e4e5e5477902a769ee7ca9ef8b03
Bvenugopal760 <boyavenugopal760@gmail.com>
2026-01-02 14:28:17 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'venuft';
+
+  user = {
+    email: '',
+    password: ''
+  };
+
+  login(form: any) {
+    console.log('Login Data:', form.value);
+
+    if (this.user.email === 'admin@gmail.com' &&
+        this.user.password === '123456') {
+      alert('Login Successful');
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 }
